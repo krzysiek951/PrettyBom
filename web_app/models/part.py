d1741425fd2faad46d4191c8ff6c72fd7b6b9e42
krzysiek951 <krzysiek951@gmail.com>
2023-01-24 16:40:22 +0100

    Refactored part classes.

--- web_app/models/part.py
+++ web_app/models/part.py
@@ -2,28 +2,35 @@ from __future__ import annotations
 
 from abc import ABC
 
+from web_app.typing import PartTypes, PartFileTypes, PartClassTypes
+
 
 class AbstractPart(ABC):
+    """Abstract class for a Part."""
+
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
-        self.sets = None
-        self.to_order = None
-        self.parent = None
-        self.child = None
-        self.parent_assembly = None
-        self.assembly_number = None
-        self.assembly_name = None
-        self.type = None  # TODO: ADD LITERAL
-        self.file_type = None  # TODO: ADD LITERAL
-        self.is_fastener = None
-        self.is_purchased = None
-        self.is_production = None
-        self.is_junk_by_keywords = None
-        self.is_junk_by_empty_fields = None
-        self.is_junk_by_purchased_part_nesting = None
-        self.is_junk = None
+        self.sets: int | None = None
+        self.to_order: int | None = None
+        self.parent: list[str] | None = None
+        self.child: list[str] | None = None
+        self.parent_assembly: str | None = None
+        self.assembly_number: str | None = None
+        self.assembly_name: str | None = None
+        self.type: PartTypes | None = None
+        self.file_type: PartFileTypes | None = None
+        self.is_fastener: bool | None = None
+        self.is_purchased: bool | None = None
+        self.is_production: bool | None = None
+        self.is_junk_by_keywords: bool | None = None
+        self.is_junk_by_empty_fields: bool | None = None
+        self.is_junk_by_purchased_part_nesting: bool | None = None
+        self.is_junk: bool | None = None
 
 
 class DefaultPart(AbstractPart):
+    """Class for default type of the Part."""
+    part_type: PartClassTypes = 'default'
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
